8d28a6bf8a7fb24e001fdbc949363445298a5aaa
Ali Rezagholizadeh <alirezagholizadeh70@gmail.com>
2022-10-03 09:54:04 -0400

    first commit

--- Projects in Matlab/Other Projects/Genetic Algorithm/SutableKnn.m
+++
@@ -1,7 +0,0 @@
-function n=SutableKnn(data)
-n=0;
-m=size(data,2);
-for i=m:-1:1
-    n=n+data(1,i)*(10^(m-i));
-end
-end
--- Projects in Matlab/Other Projects/project 5 (Fourier transform_ for enhancing quality)/PostProcessing.m
+++
@@ -1,8 +0,0 @@
-function ImDom=PostProcessing(image)
-for i=1:size(image,1)
-    for j=1:size(image,2)
-        image(i,j)=image(i,j)*((-1)^(i+j));
-    end
-end
-ImDom=image;
-end
